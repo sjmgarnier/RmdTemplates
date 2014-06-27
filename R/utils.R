a5007e875d060d2452beb76ce2626e458c850a0f
Simon Garnier <simon.garnier@gmail.com>
2014-06-27 14:11:46 +1000

    Was missing a few functions (in R/utils.R)

---
+++ R/utils.R
@@ -0,0 +1,13 @@
+find_file <- function(template, file) {
+  template <- system.file("rmarkdown", "templates", template, file,
+                          package = "rticles")
+  if (template == "") {
+    stop("Couldn't find template file ", template, "/", file, call. = FALSE)
+  }
+  
+  template
+}
+
+find_resource <- function(template, file) {
+  find_file(template, file.path("resources", file))
+}
